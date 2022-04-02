58e94dea24d306e79c0e1127e77a7d29bb0dabc7
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-04-02 14:06:12 +0530

    cmmt

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-module.exports = () => {
-  return mongoose.connect("mongodb+srv://dishu:qwerty123456@evaluation.ubmpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+module.exports = async () => {
+  return await mongoose.connect("mongodb+srv://dishu:qwerty123456@evaluation.ubmpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 };
