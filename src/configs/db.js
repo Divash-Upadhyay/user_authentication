e8286ad8578f42f5d638432f2ad6a2f7fef64fb6
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-04-02 13:29:05 +0530

    msg

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect("mongodb+srv://dishu:qwerty123456@evaluation.ubmpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"),{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-  };
+  return mongoose.connect("mongodb+srv://dishu:qwerty123456@evaluation.ubmpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 };
