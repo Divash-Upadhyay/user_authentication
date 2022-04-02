6bd7ce88fc450355d664e1e403e6486ec11562cf
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-04-02 11:50:29 +0530

    cmt

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 app.use(express.json());
 
 app.use(cors());
-app.use("/users", userController)
+app.use("/users", userController) 
 
 app.post("/register", register)
 
